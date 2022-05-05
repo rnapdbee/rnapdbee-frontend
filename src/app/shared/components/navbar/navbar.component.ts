e755750038dd51524dd41653549b18fc8515b3e6
wachuuu <gab.wachowski15@gmail.com>
2022-05-05 21:50:48 +0200

    feat: navbar

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { animate, style, transition, trigger } from '@angular/animations';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
+  animations: [
+    trigger('slideDownUp', [
+      transition(':enter', [style({ height: 0 }), animate(100)]),
+      transition(':leave', [animate(100, style({ height: 0 }))]),
+    ]),
+  ]
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent {
+  menuExpanded = false;
 
-  constructor() { }
+  constructor(private readonly router: Router) { }
 
-  ngOnInit(): void {
+  toggleMenu() {
+    this.menuExpanded = !this.menuExpanded;
   }
 
+  isActive(path: string): boolean {
+    if (this.router.url === `/${path}`) return true;
+    return false;
+  }
 }

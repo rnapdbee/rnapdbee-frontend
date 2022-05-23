05e7f0a0acb901dc9767e5c2437fd082b6727220
wachuuu <gab.wachowski15@gmail.com>
2022-05-23 17:46:32 +0200

    feat: stepper & radio button

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,17 +1,25 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
+import { MatRadioModule } from '@angular/material/radio';
 import { RouterModule } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { RadioButtonComponent } from './components/radio-button/radio-button.component';
+import { StepComponent } from './components/step/step.component';
 
 const COMPONENTS = [
   NavbarComponent,
+  StepComponent,
+  RadioButtonComponent,
 ];
 
 const MODULES = [
   CommonModule,
   MatIconModule,
   RouterModule,
+  MatRadioModule,
+  FormsModule,
 ];
 
 @NgModule({

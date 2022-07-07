d3493d50c832d4ff8a89e70bd94620329f5c25bb
wachuuu <gab.wachowski15@gmail.com>
2022-07-07 11:38:49 +0200

    chore: checkbox [(ngModel)] accessor

--- src/app/shared/components/checkbox/checkbox.component.ts
+++ src/app/shared/components/checkbox/checkbox.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from '@angular/core';
+import { Component, forwardRef, Input } from '@angular/core';
+import { NG_VALUE_ACCESSOR } from '@angular/forms';
 import { OpenCloseAnimation } from '../../animations/open-close';
 
 @Component({
@@ -6,18 +7,46 @@ import { OpenCloseAnimation } from '../../animations/open-close';
   templateUrl: './checkbox.component.html',
   styleUrls: ['./checkbox.component.scss'],
   animations: [OpenCloseAnimation],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => CheckboxComponent),
+      multi: true,
+    },
+  ]
 })
 export class CheckboxComponent {
   @Input() label = '';
-  @Input() checked = false;
   @Input() showControls = false;
   @Input() expanded = false;
-
-  onChange() {
-    this.checked = !this.checked;
-  }
+  val: boolean | undefined = undefined;
 
   toggleExpand() {
     this.expanded = !this.expanded;
+  }
+
+  onChange(_: boolean) { }
+
+  onTouch(_: boolean) { }
+
+  set value(val: boolean) {
+    if( val !== undefined && this.val !== val){
+      this.val = val;
+      this.onChange(val);
+      this.onTouch(val);
+    }
+  }
+  get value() { return !!this.val; }
+
+  writeValue(value: boolean){ 
+    this.value = value;
+  }
+
+  registerOnChange(fn: any){
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any){
+    this.onTouch = fn;
   }
 }

7af75a2eddde5cdae25d6cf0373fb8cc8d9e0abd
wachuuu <gab.wachowski15@gmail.com>
2022-07-07 12:29:54 +0200

    feat: complete tertiary-to-dbn input form

--- src/app/calculations/components/teriary-to-dbn-input-form/teriary-to-dbn-input-form.component.ts
+++ src/app/calculations/components/teriary-to-dbn-input-form/teriary-to-dbn-input-form.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { AnalisysTool, ModelSelection, NonCanonicalHandling, StructuralElementsHandling, VisualizationTool } from 'src/app/shared/constants/param-options.const';
+import { TERIARY_TO_DBN_EXAMPLES } from 'src/app/shared/constants/teriary-to-dbn-examples.const';
+import { Example } from 'src/app/shared/models/example.model';
 
 export enum UploadMethod {
   fromPDB,
@@ -12,30 +16,61 @@ export enum UploadMethod {
   styleUrls: ['./teriary-to-dbn-input-form.component.scss']
 })
 export class TeriaryToDBNInputFormComponent {
+  examples = TERIARY_TO_DBN_EXAMPLES;
   UploadMethod: typeof UploadMethod = UploadMethod;
+  ModelSelection = ModelSelection;
+  AnalisysTool = AnalisysTool;
+  NonCanonicalHandling = NonCanonicalHandling;
+  StructuralElementsHandling = StructuralElementsHandling;
+  VisualizationTool = VisualizationTool;
   
-  get uploadMethod() { return this._upload; }
-  set uploadMethod(value: UploadMethod) { this._upload = value; }
-  private _upload = UploadMethod.fromPDB
+  constructor(private readonly fb: FormBuilder) {}
 
+  uploadMethod = UploadMethod.fromPDB;
+  
   get pdbId() { return this._pdbId; }
-  set pdbId(value: string | null) { this._pdbId = value; }
+  set pdbId(value: string | null) { this.setAndValidateId(value); }
   private _pdbId: string | null = null;
+  _pdbIdError = '';
+  
+  file: File | null = null;
+  _fileError = '';
+  
+  example: Example | null = null;
 
-  get file() { return this._file; }
-  set file(value: File | null) { this._file = value; }
-  private _file: File | null = null;
-
-
+  paramsForm = this.fb.group({
+    modelSelection: [ModelSelection[0].key],
+    analisysTool: [AnalisysTool[0].key],
+    nonCanonicalHandling: [NonCanonicalHandling[0].key],
+    removeIsolated: [true],
+    structuralElementsHandling: [StructuralElementsHandling[0].key],
+    visualizationTool: [VisualizationTool[0].key],
+  });
+  
   uploadFile(event: Event) {
-    let files = (event.target as HTMLInputElement).files
+    let files = (event.target as HTMLInputElement).files;
     if (files && files?.length > 0) {
-      let file = files[0]
-      this.file = file;
+      let file = files[0];
+      this.setAndValidateFile(file);
     }
   }
 
+  setAndValidateId(value: string | null) {
+    // TODO: implement validation for pdbId
+    this._pdbId = value;
+  }
+  
+  setAndValidateFile(file: File) {
+    // TODO: implement validation for file
+    this.file = file;
+  }
+
+  onExampleSelect(event: Example) {
+    this.example = event;
+  }
+  
   onSubmit() {
-    console.log(this.uploadMethod)
+    // TODO: submit form to service
+    console.log(this.paramsForm.value);
   }
 }

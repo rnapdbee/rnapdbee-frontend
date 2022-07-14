8dbdd73b589ee9c66dead7b86ab69699a82238e4
Gabriel Wachowski <gabriel.wachowski@primacode.io>
2022-07-14 16:19:32 +0200

    refactor: simplify example assets paths

--- src/app/shared/constants/dbn-to-image-examples.const.ts
+++ src/app/shared/constants/dbn-to-image-examples.const.ts
@@ -4,16 +4,16 @@ export const DBN_TO_IMAGE_EXAMPLES: Example[] = [
   {
     no: 1,
     name: '1EHZ.dbn',
-    path: 'assets/examples/dbn-to-image/1EHZ.dbn'
+    path: 'assets/examples/dbn/1EHZ.dbn'
   },
   {
     no: 2,
     name: '2Z74.dbn',
-    path: 'assets/examples/dbn-to-image/2Z74.dbn'
+    path: 'assets/examples/dbn/2Z74.dbn'
   },
   {
     no: 3,
     name: '3G78.dbn',
-    path: 'assets/examples/dbn-to-image/3G78.dbn'
+    path: 'assets/examples/dbn/3G78.dbn'
   },
 ];
--- src/app/shared/constants/tertiary-to-dbn-examples.const.ts
+++ src/app/shared/constants/tertiary-to-dbn-examples.const.ts
@@ -4,31 +4,31 @@ export const TERTIARY_TO_DBN_EXAMPLES: Example[] = [
   {
     no: 1,
     name: '1EHZ.cif',
-    path: 'assets/examples/tertiary-to-dbn/1EHZ.cif'
+    path: 'assets/examples/tertiary/1EHZ.cif'
   },
   {
     no: 2,
     name: '2Z74.cif',
-    path: 'assets/examples/tertiary-to-dbn/2Z74.cif'
+    path: 'assets/examples/tertiary/2Z74.cif'
   },
   {
     no: 3,
     name: '3G78.cif',
-    path: 'assets/examples/tertiary-to-dbn/3G78.cif'
+    path: 'assets/examples/tertiary/3G78.cif'
   },
   {
     no: 4,
     name: '2LBK.cif',
-    path: 'assets/examples/tertiary-to-dbn/2LBK.cif'
+    path: 'assets/examples/tertiary/2LBK.cif'
   },
   {
     no: 5,
     name: '3J7Y-A.cif',
-    path: 'assets/examples/tertiary-to-dbn/3J7Y-A.cif'
+    path: 'assets/examples/tertiary/3J7Y-A.cif'
   },
   {
     no: 6,
     name: '2KBP.cif',
-    path: 'assets/examples/tertiary-to-dbn/2KBP.cif'
+    path: 'assets/examples/tertiary/2KBP.cif'
   },
 ];

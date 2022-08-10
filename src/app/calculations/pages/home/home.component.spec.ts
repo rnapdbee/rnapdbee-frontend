4f3a9e53d66a22c1258a663794d795dd2c554317
Gabriel Wachowski <gabriel.wachowski@primacode.io>
2022-08-10 13:53:53 +0200

    feat: unit tests for HomeComponent

---
+++ src/app/calculations/pages/home/home.component.spec.ts
@@ -0,0 +1,65 @@
+import { DebugElement, NO_ERRORS_SCHEMA } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { CalculationType } from 'src/app/shared/models/calculation-type.enum';
+import { HomeComponent } from './home.component';
+
+const tertiaryToDBNInputForm = 'app-tertiary-to-dbn-input-form';
+const secondaryToDBNInputForm = 'app-secondary-to-dbn-input-form';
+const dbnToImageInputForm = 'app-dbn-to-image-input-form';
+const tertiaryToMultiInputForm = 'app-tertiary-to-multi-input-form';
+
+describe('HomeComponent', () => {
+  let fixture: ComponentFixture<HomeComponent>;
+  let component: HomeComponent;
+  let debugElement: DebugElement;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [HomeComponent],
+      schemas: [NO_ERRORS_SCHEMA],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(HomeComponent);
+    component = fixture.componentInstance;
+    ({ debugElement } = fixture);
+    fixture.detectChanges();
+  });
+
+  it('renders TertiaryToDBNInputForm by default', () => {
+    const form = debugElement.query(By.css(tertiaryToDBNInputForm));
+    const calcType = component.calculationType;
+    expect(form).toBeTruthy();
+    expect(calcType).toEqual(CalculationType.TertiaryToDBN);
+  });
+
+  describe('CalculationType', () => {
+    it('renders TertiaryToDBNInputForm when set to TertiaryToDBN', () => {
+      component.setCalculationType(CalculationType.TertiaryToDBN);
+      fixture.detectChanges();
+      const form = debugElement.query(By.css(tertiaryToDBNInputForm));
+      expect(form).toBeTruthy();
+    });
+
+    it('renders SecondaryToDBNInputForm when set to SecondaryToDBN', () => {
+      component.setCalculationType(CalculationType.SecondaryToDBN);
+      fixture.detectChanges();
+      const form = debugElement.query(By.css(secondaryToDBNInputForm));
+      expect(form).toBeTruthy();
+    });
+
+    it('renders DBNToImageInputForm when set to DBNToImage', () => {
+      component.setCalculationType(CalculationType.DBNToImage);
+      fixture.detectChanges();
+      const form = debugElement.query(By.css(dbnToImageInputForm));
+      expect(form).toBeTruthy();
+    });
+
+    it('renders TertiaryToMultiInputForm when set to TertiaryToMultiSecondary', () => {
+      component.setCalculationType(CalculationType.TertiaryToMultiSecondary);
+      fixture.detectChanges();
+      const form = debugElement.query(By.css(tertiaryToMultiInputForm));
+      expect(form).toBeTruthy();
+    });
+  });
+});

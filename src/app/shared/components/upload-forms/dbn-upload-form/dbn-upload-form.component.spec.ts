2ff3b8e12b878e5eb2e0902fb775c8a70ff76c1e
Gabriel Wachowski <gab.wachowski15@gmail.com>
2022-08-18 11:37:17 +0200

    feat: unit tests for HomeComponent & upload-forms (#10)
    * feat: unit tests for HomeComponent
    
    * feat: unit tests for home upload-forms

---
+++ src/app/shared/components/upload-forms/dbn-upload-form/dbn-upload-form.component.spec.ts
@@ -0,0 +1,144 @@
+/* eslint-disable @typescript-eslint/unbound-method */
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DBN_TO_IMAGE_EXAMPLES } from 'src/app/shared/constants/dbn-to-image-examples.const';
+import { Example } from 'src/app/shared/models/example.model';
+import { UploadMethodType } from 'src/app/shared/models/upload-type.model';
+import { DbnUploadFormComponent } from './dbn-upload-form.component';
+
+
+describe('DbnUploadFormComponent', () => {
+  let fixture: ComponentFixture<DbnUploadFormComponent>;
+  let component: DbnUploadFormComponent;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [DbnUploadFormComponent],
+      schemas: [NO_ERRORS_SCHEMA],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(DbnUploadFormComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('emits invalid empty payload on init', () => {
+    spyOn(component.uploadChange, 'emit');
+    component.notifyChanges();
+    expect(component.uploadChange.emit).toHaveBeenCalledOnceWith(jasmine.objectContaining({
+      data: null,
+      valid: false,
+    }));
+  });
+
+  it('not emits when uploadMethod is out of scope', () => {
+    spyOn(component.uploadChange, 'emit');
+    component.currentUploadType = UploadMethodType.fromPDB;
+    component.notifyChanges();
+    expect(component.uploadChange.emit).toHaveBeenCalledTimes(0);
+  });
+
+  describe('UploadMethod change', () => {
+    beforeEach(() => {
+      spyOn(component.uploadChange, 'emit');
+    });
+
+    it('emits object of type file when set to file', () => {
+      component.currentUploadType = UploadMethodType.fromLocalFile;
+      component.onMethodChange();
+      expect(component.uploadChange.emit).toHaveBeenCalledWith(jasmine.objectContaining({
+        type: UploadMethodType.fromLocalFile,
+      }));
+    });
+
+    it('emits object of type example when set to example', () => {
+      component.currentUploadType = UploadMethodType.fromExample;
+      component.onMethodChange();
+      expect(component.uploadChange.emit).toHaveBeenCalledWith(jasmine.objectContaining({
+        type: UploadMethodType.fromExample,
+      }));
+    });
+  });
+
+  describe('File upload method', () => {
+    let mockFile: File;
+
+    beforeEach(() => {
+      component.currentUploadType = UploadMethodType.fromLocalFile;
+      mockFile = new File([], 'mocked file');
+      spyOn(component.uploadChange, 'emit');
+    });
+
+    it('emits invalid payload when file not provided', () => {
+      component.file = null;
+      component.notifyChanges();
+      expect(component.uploadChange.emit).toHaveBeenCalledWith(jasmine.objectContaining({
+        valid: false,
+      }));
+    });
+
+    // TODO: it('emits invalid payload when file provided and validation fails', () => {});
+
+    // TODO: it('emits valid payload when file provided and validation passes', () => {});
+
+    it('emits new value when file changed', () => {
+      const mockFile2 = new File([], 'mocked file 2');
+      component.setAndValidateFile(mockFile);
+      component.setAndValidateFile(mockFile2);
+      expect(component.uploadChange.emit).toHaveBeenCalledTimes(2);
+    });
+
+    it('includes file object in payload', () => {
+      component.setAndValidateFile(mockFile);
+      expect(component.uploadChange.emit).toHaveBeenCalledWith(jasmine.objectContaining({
+        data: mockFile,
+      }));
+    });
+  });
+
+  describe('Example upload method', () => {
+    let mockExample: Example;
+
+    beforeEach(() => {
+      component.currentUploadType = UploadMethodType.fromExample;
+      mockExample = DBN_TO_IMAGE_EXAMPLES[0];
+      spyOn(component.uploadChange, 'emit');
+    });
+
+    it('renders example picker', () => {
+      const { debugElement } = fixture;
+      const picker = debugElement.query(By.css('app-example-picker'));
+      expect(picker).toBeTruthy();
+    });
+
+    it('emits invalid payload when example not provided', () => {
+      component.example = null;
+      component.notifyChanges();
+      expect(component.uploadChange.emit).toHaveBeenCalledWith(jasmine.objectContaining({
+        valid: false,
+      }));
+    });
+
+    it('emits valid payload when example provided', () => {
+      component.onExampleSelect(mockExample);
+      expect(component.uploadChange.emit).toHaveBeenCalledWith(jasmine.objectContaining({
+        valid: true,
+      }));
+    });
+
+    it('emits new value when example changed', () => {
+      const mockedExample2 = DBN_TO_IMAGE_EXAMPLES[1];
+      component.onExampleSelect(mockExample);
+      component.onExampleSelect(mockedExample2);
+      expect(component.uploadChange.emit).toHaveBeenCalledTimes(2);
+    });
+
+    it('includes example object in payload', () => {
+      component.onExampleSelect(mockExample);
+      expect(component.uploadChange.emit).toHaveBeenCalledWith(jasmine.objectContaining({
+        data: mockExample,
+      }));
+    });
+  });
+});

36eccaf601750afd3bbc00bdc90b38ce7b40346d
Gabriel Wachowski <gab.wachowski15@gmail.com>
2022-08-18 11:37:17 +0200

    feat: unit tests for HomeComponent & upload-forms (#10)
    * feat: unit tests for HomeComponent
    
    * feat: unit tests for home upload-forms

---
+++ src/app/shared/components/upload-forms/secondary-upload-form/secondary-upload-form.component.spec.ts
@@ -0,0 +1,208 @@
+/* eslint-disable @typescript-eslint/unbound-method */
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { SECONDARY_TO_DBN_BPSEQ_EXAMPLES, SECONDARY_TO_DBN_CT_EXAMPLES } from 'src/app/shared/constants/secondary-to-dbn-examples.const';
+import { Example } from 'src/app/shared/models/example.model';
+import { UploadMethodType } from 'src/app/shared/models/upload-type.model';
+import { SecondaryUploadFormComponent } from './secondary-upload-form.component';
+
+describe('SecondaryUploadFormComponent', () => {
+  let fixture: ComponentFixture<SecondaryUploadFormComponent>;
+  let component: SecondaryUploadFormComponent;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [SecondaryUploadFormComponent],
+      schemas: [NO_ERRORS_SCHEMA],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(SecondaryUploadFormComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('emits invalid empty payload on init', () => {
+    spyOn(component.uploadChange, 'emit');
+    component.notifyChanges();
+    expect(component.uploadChange.emit).toHaveBeenCalledOnceWith(jasmine.objectContaining({
+      data: null,
+      valid: false,
+    }));
+  });
+
+  describe('UploadMethod change', () => {
+    beforeEach(() => {
+      spyOn(component.uploadChange, 'emit');
+    });
+
+    it('emits object of type file when set to file', () => {
+      component.currentUploadType = UploadMethodType.fromLocalFile;
+      component.onMethodChange();
+      expect(component.uploadChange.emit).toHaveBeenCalledWith(jasmine.objectContaining({
+        type: UploadMethodType.fromLocalFile,
+      }));
+    });
+
+    it('emits object of type example when set to example', () => {
+      component.currentUploadType = UploadMethodType.fromExample;
+      component.onMethodChange();
+      expect(component.uploadChange.emit).toHaveBeenCalledWith(jasmine.objectContaining({
+        type: UploadMethodType.fromExample,
+      }));
+    });
+  });
+
+  describe('File upload method', () => {
+    let mockFile: File;
+
+    beforeEach(() => {
+      component.currentUploadType = UploadMethodType.fromLocalFile;
+      mockFile = new File([], 'mocked file');
+      spyOn(component.uploadChange, 'emit');
+    });
+
+    it('emits invalid payload when file not provided', () => {
+      component.file = null;
+      component.notifyChanges();
+      expect(component.uploadChange.emit).toHaveBeenCalledWith(jasmine.objectContaining({
+        valid: false,
+      }));
+    });
+
+    // TODO: it('emits invalid payload when file provided and validation fails', () => {});
+
+    // TODO: it('emits valid payload when file provided and validation passes', () => {});
+
+    it('emits new value when file changed', () => {
+      const mockFile2 = new File([], 'mocked file 2');
+      component.setAndValidateFile(mockFile);
+      component.setAndValidateFile(mockFile2);
+      expect(component.uploadChange.emit).toHaveBeenCalledTimes(2);
+    });
+
+    it('includes file object in payload', () => {
+      component.setAndValidateFile(mockFile);
+      expect(component.uploadChange.emit).toHaveBeenCalledWith(jasmine.objectContaining({
+        data: mockFile,
+      }));
+    });
+  });
+
+  describe('Example upload method', () => {
+    beforeEach(() => {
+      component.currentUploadType = UploadMethodType.fromExample;
+    });
+
+    it('renders example picker', () => {
+      const { debugElement } = fixture;
+      const picker = debugElement.query(By.css('app-example-picker'));
+      expect(picker).toBeTruthy();
+    });
+
+    it('emits new payload when example type changed', () => {
+      spyOn(component.uploadChange, 'emit');
+      component.onExampleTypeChange('bpseq');
+      component.onExampleTypeChange('ct');
+      expect(component.uploadChange.emit).toHaveBeenCalledTimes(2);
+    });
+
+    describe('Bpseq example type', () => {
+      let mockExample: Example;
+
+      beforeEach(() => {
+        component.onExampleTypeChange('bpseq');
+        mockExample = SECONDARY_TO_DBN_BPSEQ_EXAMPLES[0];
+        spyOn(component.uploadChange, 'emit');
+      });
+
+      it('emits invalid payload when bpseq example not provided', () => {
+        component.bpseqExample = null;
+        component.notifyChanges();
+        expect(component.uploadChange.emit).toHaveBeenCalledWith(jasmine.objectContaining({
+          valid: false,
+        }));
+      });
+
+      it('emits valid payload when bpseq example provided', () => {
+        component.onBpseqExampleSelect(mockExample);
+        expect(component.uploadChange.emit).toHaveBeenCalledWith(jasmine.objectContaining({
+          valid: true,
+        }));
+      });
+
+      it('emits new value when example changed', () => {
+        const mockedExample2 = SECONDARY_TO_DBN_BPSEQ_EXAMPLES[1];
+        component.onBpseqExampleSelect(mockExample);
+        component.onBpseqExampleSelect(mockedExample2);
+        expect(component.uploadChange.emit).toHaveBeenCalledTimes(2);
+      });
+
+      it('includes example object in payload', () => {
+        component.onBpseqExampleSelect(mockExample);
+        expect(component.uploadChange.emit).toHaveBeenCalledWith(jasmine.objectContaining({
+          data: mockExample,
+        }));
+      });
+
+      it('emits bpseq example', () => {
+        const dummyExample = { name: '', no: -1, path: '' };
+        component.bpseqExample = mockExample;
+        component.ctExample = dummyExample;
+        component.notifyChanges();
+        expect(component.uploadChange.emit).toHaveBeenCalledWith(jasmine.objectContaining({
+          data: component.bpseqExample,
+        }));
+      });
+    });
+
+    describe('Ct example type', () => {
+      let mockExample: Example;
+
+      beforeEach(() => {
+        component.onExampleTypeChange('ct');
+        mockExample = SECONDARY_TO_DBN_CT_EXAMPLES[0];
+        spyOn(component.uploadChange, 'emit');
+      });
+
+      it('emits invalid payload when ct example not provided', () => {
+        component.ctExample = null;
+        component.notifyChanges();
+        expect(component.uploadChange.emit).toHaveBeenCalledWith(jasmine.objectContaining({
+          valid: false,
+        }));
+      });
+
+      it('emits valid payload when ct example provided', () => {
+        component.onCtExampleSelect(mockExample);
+        expect(component.uploadChange.emit).toHaveBeenCalledWith(jasmine.objectContaining({
+          valid: true,
+        }));
+      });
+
+      it('emits new value when example changed', () => {
+        const mockedExample2 = SECONDARY_TO_DBN_CT_EXAMPLES[1];
+        component.onBpseqExampleSelect(mockExample);
+        component.onBpseqExampleSelect(mockedExample2);
+        expect(component.uploadChange.emit).toHaveBeenCalledTimes(2);
+      });
+
+      it('includes example object in payload', () => {
+        component.onCtExampleSelect(mockExample);
+        expect(component.uploadChange.emit).toHaveBeenCalledWith(jasmine.objectContaining({
+          data: mockExample,
+        }));
+      });
+
+      it('emits ct example', () => {
+        const dummyExample = { name: '', no: -1, path: '' };
+        component.ctExample = mockExample;
+        component.bpseqExample = dummyExample;
+        component.notifyChanges();
+        expect(component.uploadChange.emit).toHaveBeenCalledWith(jasmine.objectContaining({
+          data: component.ctExample,
+        }));
+      });
+    });
+  });
+});
---
+++ src/app/shared/components/upload-forms/tertiary-upload-form/tertiary-upload-form.component.spec.ts
@@ -0,0 +1,179 @@
+/* eslint-disable @typescript-eslint/unbound-method */
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { TERTIARY_TO_DBN_EXAMPLES } from 'src/app/shared/constants/tertiary-to-dbn-examples.const';
+import { Example } from 'src/app/shared/models/example.model';
+import { UploadMethodType } from 'src/app/shared/models/upload-type.model';
+import { TertiaryUploadFormComponent } from './tertiary-upload-form.component';
+
+describe('TertiaryUploadFormComponent', () => {
+  let fixture: ComponentFixture<TertiaryUploadFormComponent>;
+  let component: TertiaryUploadFormComponent;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [TertiaryUploadFormComponent],
+      schemas: [NO_ERRORS_SCHEMA],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(TertiaryUploadFormComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('emits invalid empty payload on init', () => {
+    spyOn(component.uploadChange, 'emit');
+    component.notifyChanges();
+    expect(component.uploadChange.emit).toHaveBeenCalledOnceWith(jasmine.objectContaining({
+      data: '',
+      valid: false,
+    }));
+  });
+
+  describe('UploadMethod change', () => {
+    beforeEach(() => {
+      spyOn(component.uploadChange, 'emit');
+    });
+
+    it('emits object of type pdb when set to pdb', () => {
+      component.currentUploadType = UploadMethodType.fromPDB;
+      component.onMethodChange();
+      expect(component.uploadChange.emit).toHaveBeenCalledWith(jasmine.objectContaining({
+        type: UploadMethodType.fromPDB,
+      }));
+    });
+
+    it('emits object of type file when set to file', () => {
+      component.currentUploadType = UploadMethodType.fromLocalFile;
+      component.onMethodChange();
+      expect(component.uploadChange.emit).toHaveBeenCalledWith(jasmine.objectContaining({
+        type: UploadMethodType.fromLocalFile,
+      }));
+    });
+
+    it('emits object of type example when set to example', () => {
+      component.currentUploadType = UploadMethodType.fromExample;
+      component.onMethodChange();
+      expect(component.uploadChange.emit).toHaveBeenCalledWith(jasmine.objectContaining({
+        type: UploadMethodType.fromExample,
+      }));
+    });
+  });
+
+  describe('PdbId upload method', () => {
+    let mockPdbId: string;
+
+    beforeEach(() => {
+      component.currentUploadType = UploadMethodType.fromPDB;
+      mockPdbId = '4R30';
+      spyOn(component.uploadChange, 'emit');
+    });
+
+    it('emits invalid payload when pdbId not provided', () => {
+      component.pdbId = '';
+      expect(component.uploadChange.emit).toHaveBeenCalledWith(jasmine.objectContaining({
+        valid: false,
+      }));
+    });
+
+    // TODO: it('emits invalid payload when pdbId provided and validation fails', () => {});
+
+    // TODO: it('emits valid payload when pdbId provided and validation passes', () => {});
+
+    it('emits new value when file changed', () => {
+      const mockPdbId2 = 'XXXX';
+      component.pdbId = mockPdbId;
+      component.pdbId = mockPdbId2;
+      expect(component.uploadChange.emit).toHaveBeenCalledTimes(2);
+    });
+
+    it('includes pdbId in payload', () => {
+      component.pdbId = mockPdbId;
+      expect(component.uploadChange.emit).toHaveBeenCalledWith(jasmine.objectContaining({
+        data: mockPdbId,
+      }));
+    });
+  });
+
+  describe('File upload method', () => {
+    let mockFile: File;
+
+    beforeEach(() => {
+      component.currentUploadType = UploadMethodType.fromLocalFile;
+      mockFile = new File([], 'mocked file');
+      spyOn(component.uploadChange, 'emit');
+    });
+
+    it('emits invalid payload when file not provided', () => {
+      component.file = null;
+      component.notifyChanges();
+      expect(component.uploadChange.emit).toHaveBeenCalledWith(jasmine.objectContaining({
+        valid: false,
+      }));
+    });
+
+    // TODO: it('emits invalid payload when file provided and validation fails', () => {});
+
+    // TODO: it('emits valid payload when file provided and validation passes', () => {});
+
+    it('emits new value when file changed', () => {
+      const mockFile2 = new File([], 'mocked file 2');
+      component.setAndValidateFile(mockFile);
+      component.setAndValidateFile(mockFile2);
+      expect(component.uploadChange.emit).toHaveBeenCalledTimes(2);
+    });
+
+    it('includes file object in payload', () => {
+      component.setAndValidateFile(mockFile);
+      expect(component.uploadChange.emit).toHaveBeenCalledWith(jasmine.objectContaining({
+        data: mockFile,
+      }));
+    });
+  });
+
+  describe('Example upload method', () => {
+    let mockExample: Example;
+
+    beforeEach(() => {
+      component.currentUploadType = UploadMethodType.fromExample;
+      mockExample = TERTIARY_TO_DBN_EXAMPLES[0];
+      spyOn(component.uploadChange, 'emit');
+    });
+
+    it('renders example picker', () => {
+      const { debugElement } = fixture;
+      const picker = debugElement.query(By.css('app-example-picker'));
+      expect(picker).toBeTruthy();
+    });
+
+    it('emits invalid payload when example not provided', () => {
+      component.example = null;
+      component.notifyChanges();
+      expect(component.uploadChange.emit).toHaveBeenCalledWith(jasmine.objectContaining({
+        valid: false,
+      }));
+    });
+
+    it('emits valid payload when example provided', () => {
+      component.onExampleSelect(mockExample);
+      expect(component.uploadChange.emit).toHaveBeenCalledWith(jasmine.objectContaining({
+        valid: true,
+      }));
+    });
+
+    it('emits new value when example changed', () => {
+      const mockedExample2 = TERTIARY_TO_DBN_EXAMPLES[1];
+      component.onExampleSelect(mockExample);
+      component.onExampleSelect(mockedExample2);
+      expect(component.uploadChange.emit).toHaveBeenCalledTimes(2);
+    });
+
+    it('includes example object in payload', () => {
+      component.onExampleSelect(mockExample);
+      expect(component.uploadChange.emit).toHaveBeenCalledWith(jasmine.objectContaining({
+        data: mockExample,
+      }));
+    });
+  });
+});

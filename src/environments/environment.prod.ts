1776dd2b4da322c3753b8d86da5e5d2115e4c7af
Gabriel Wachowski <gabriel.wachowski@primacode.io>
2022-09-15 17:09:50 +0200

    fix: add baseUrl to environment.prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
   production: true,
+  baseUrl: 'http://localhost:4010/api/v1/engine',
 };
+
+export enum ApiPaths {
+  Tertiary = '/3d/',
+  Secondary = '/2d/',
+  Image = '/image/',
+  Multi = '/multi/'
+}

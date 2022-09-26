f5aa9187f57f0729b5168011fa9faf701438d237
Gabriel Wachowski <gab.wachowski15@gmail.com>
2022-09-26 18:04:38 +0200

    feat: add example files to the assets (#36)
    * feat: add example files to the assets
    
    * chore: add newlines to example files

--- src/app/shared/constants/dbn-to-image-examples.const.ts
+++ src/app/shared/constants/dbn-to-image-examples.const.ts
@@ -4,16 +4,16 @@ export const DBN_TO_IMAGE_EXAMPLES: Example[] = [
   {
     no: 1,
     name: '1EHZ.dbn',
-    path: 'assets/examples/dbn/1EHZ.dbn',
+    path: 'assets/examples/secondary/1EHZ.dbn',
   },
   {
     no: 2,
     name: '2Z74.dbn',
-    path: 'assets/examples/dbn/2Z74.dbn',
+    path: 'assets/examples/secondary/2Z74.dbn',
   },
   {
     no: 3,
     name: '3G78.dbn',
-    path: 'assets/examples/dbn/3G78.dbn',
+    path: 'assets/examples/secondary/3G78.dbn',
   },
 ];

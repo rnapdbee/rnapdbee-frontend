c83553987457ea78323d70fea89b8c6e1f091406
Gabriel Wachowski <wachowski.gabriel@gmail.com>
2022-12-20 12:30:48 +0100

    feat: add tests to multi results

---
+++ src/app/calculations/components/tertiary-to-multi-results/tertiary-to-multi-results.component.spec.ts
@@ -0,0 +1,151 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Calculation } from 'src/app/shared/models/calculation/calculation.model';
+import { MultiOutput } from 'src/app/shared/models/output/multi-output.model';
+import { DrawingResult, SecondaryOutput } from 'src/app/shared/models/output/secondary-output.model';
+import { TertiaryToMultiParams } from 'src/app/shared/models/params/tertiary-to-multi-params.model';
+import { MultiSelect } from 'src/app/shared/models/select/multi-select.model';
+import { TertiaryToMultiResultsComponent } from './tertiary-to-multi-results.component';
+
+const filename = '1EHZ.cif';
+const mockUuid = 'mock-uuid-1234-5678';
+
+const mockParams: TertiaryToMultiParams = {
+  includeNonCanonical: true,
+  removeIsolated: true,
+  structuralElementsHandling: 'USE_PSEUDOKNOTS',
+  visualizationTool: 'VARNA',
+};
+
+const mock2DOutput: SecondaryOutput = {
+  strands: [{ name: '', sequence: '', structure: '' }],
+  bpSeq: [''],
+  ct: [''],
+  interactions: [''],
+  structuralElements: {
+    stems: [''],
+    loops: [''],
+    singleStrands: [''],
+    singleStrands5p: [''],
+    singleStrands3p: [''],
+  },
+  imageInformation: {
+    pathToSVGImage: '',
+    successfulVisualizationTool: '',
+    failedVisualizationTool: '',
+    drawingResult: DrawingResult.DoneByMainDrawer,
+  },
+};
+
+const mockOutput: MultiOutput = {
+  title: '',
+  consensualVisualization: {
+    pathToSVGImage: '',
+  },
+  entries: [
+    {
+      output2D: mock2DOutput,
+      adapterEnums: ['MC_ANNOTATE, RNAPOLIS'],
+    },
+  ],
+};
+
+const mockResponse: Calculation<TertiaryToMultiParams, MultiOutput> = {
+  id: mockUuid,
+  filename,
+  results: [
+    {
+      params: mockParams,
+      output: mockOutput,
+    },
+    {
+      params: mockParams,
+      output: mockOutput,
+    },
+  ],
+};
+
+describe('TertiaryToMultiResultsComponent', () => {
+  let fixture: ComponentFixture<TertiaryToMultiResultsComponent>;
+  let component: TertiaryToMultiResultsComponent;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [HttpClientModule],
+      declarations: [TertiaryToMultiResultsComponent],
+      schemas: [NO_ERRORS_SCHEMA],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(TertiaryToMultiResultsComponent);
+    component = fixture.componentInstance;
+    component.calculation = mockResponse;
+    fixture.detectChanges();
+  });
+
+  it('ensures that every result has its selection object instance', () => {
+    expect(component.selected).toBeInstanceOf(MultiSelect);
+    expect(component.selected?.fields.length).toEqual(mockResponse.results.length);
+  });
+
+  it('updates selection objects when calculation results changes', () => {
+    const mockWithOneResult = JSON.parse(JSON.stringify(mockResponse)) as Calculation<TertiaryToMultiParams, MultiOutput>;
+    mockWithOneResult.results = [mockWithOneResult.results[0]];
+
+    component.calculation = mockWithOneResult;
+    expect(component.selected?.fields.length).toEqual(mockWithOneResult.results.length);
+
+    component.calculation = mockResponse;
+    expect(component.selected?.fields.length).toEqual(mockResponse.results.length);
+  });
+
+  describe('All results', () => {
+    beforeEach(() => {
+      component.selected?.fields.forEach(result => {
+        result.fields.consensualVisualization.activateField();
+        result.fields.entries.fields.forEach(element => {
+          element.fields.output2D.fields.bpSeq.activateField();
+          element.fields.output2D.fields.ct.activateField();
+          element.fields.output2D.fields.strands.activateField();
+          element.fields.output2D.fields.imageInformation.activateField();
+          element.fields.output2D.fields.structuralElements.activateField();
+        });
+      });
+    });
+
+    it('are selected when selectAll clicked', () => {
+      component.selectAll();
+      expect(component.selected?.isSelectedOrUnactive()).toBeTrue();
+      expect(component.allSelected()).toBeTrue();
+    });
+
+    it('are selected when every result selected', () => {
+      component.selected?.fields.forEach(item => {
+        item.set(true);
+      });
+      expect(component.selected?.isSelectedOrUnactive()).toBeTrue();
+      expect(component.allSelected()).toBeTrue();
+    });
+
+    it('are not fully selected when selectAll clicked and some results unselected', () => {
+      component.selectAll();
+      component.selected?.fields[0].set(false);
+      expect(component.selected?.isSelectedOrUnactive()).toBeFalse();
+      expect(component.allSelected()).toBeFalse();
+    });
+
+    it('are not fully selected when not all results selected', () => {
+      component.selected?.fields.forEach(item => {
+        item.set(true);
+      });
+      component.selected?.fields[0].set(false);
+      expect(component.allSelected()).toBeFalse();
+    });
+
+    it('are not selected when selectAll and then unselectAll clicked', () => {
+      component.selectAll();
+      component.selectAll();
+      expect(component.allSelected()).toBeFalse();
+    });
+  });
+});
---
+++ src/app/calculations/pages/multi-page/multi-page.component.spec.ts
@@ -0,0 +1,111 @@
+import { DebugElement, NO_ERRORS_SCHEMA } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { ActivatedRoute } from '@angular/router';
+import { of, throwError } from 'rxjs';
+import { Calculation } from 'src/app/shared/models/calculation/calculation.model';
+import { MultiOutput } from 'src/app/shared/models/output/multi-output.model';
+import { DrawingResult, SecondaryOutput } from 'src/app/shared/models/output/secondary-output.model';
+import { TertiaryToMultiParams } from 'src/app/shared/models/params/tertiary-to-multi-params.model';
+import { CalculationRequestService } from 'src/app/shared/services/calculation/calculation-request.service';
+import { TertiaryToMultiService } from 'src/app/shared/services/calculation/tertiary-to-multi.service';
+import { MultiPageComponent } from './multi-page.component';
+
+const filename = '1EHZ.cif';
+const mockUuid = 'mock-uuid-1234-5678';
+
+const mockParams: TertiaryToMultiParams = {
+  includeNonCanonical: true,
+  removeIsolated: true,
+  structuralElementsHandling: 'USE_PSEUDOKNOTS',
+  visualizationTool: 'VARNA',
+};
+
+const mock2DOutput: SecondaryOutput = {
+  strands: [{ name: '', sequence: '', structure: '' }],
+  bpSeq: [''],
+  ct: [''],
+  interactions: [''],
+  structuralElements: {
+    stems: [''],
+    loops: [''],
+    singleStrands: [''],
+    singleStrands5p: [''],
+    singleStrands3p: [''],
+  },
+  imageInformation: {
+    pathToSVGImage: '',
+    successfulVisualizationTool: '',
+    failedVisualizationTool: '',
+    drawingResult: DrawingResult.DoneByMainDrawer,
+  },
+};
+
+const mockOutput: MultiOutput = {
+  title: '',
+  consensualVisualization: {
+    pathToSVGImage: '',
+  },
+  entries: [
+    {
+      output2D: mock2DOutput,
+      adapterEnums: ['MC_ANNOTATE, RNAPOLIS'],
+    },
+  ],
+};
+
+const mockResponse: Calculation<TertiaryToMultiParams, MultiOutput> = {
+  id: mockUuid,
+  filename,
+  results: [
+    {
+      params: mockParams,
+      output: mockOutput,
+    },
+  ],
+};
+
+describe('MultiPageComponent', () => {
+  let fixture: ComponentFixture<MultiPageComponent>;
+  let component: MultiPageComponent;
+  let calculationServiceSpy: jasmine.SpyObj<CalculationRequestService<TertiaryToMultiParams, MultiOutput>>;
+  let debugElement: DebugElement;
+
+  beforeEach(async () => {
+    calculationServiceSpy = jasmine.createSpyObj<CalculationRequestService<TertiaryToMultiParams, MultiOutput>>(
+      'TertiaryToMultiService',
+      ['find'],
+    );
+
+    await TestBed.configureTestingModule({
+      declarations: [MultiPageComponent],
+      schemas: [NO_ERRORS_SCHEMA],
+      providers: [
+        { provide: ActivatedRoute, useValue: { params: of({ id: mockUuid }) } },
+        { provide: TertiaryToMultiService, useValue: calculationServiceSpy },
+      ],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(MultiPageComponent);
+    component = fixture.componentInstance;
+    ({ debugElement } = fixture);
+    fixture.detectChanges();
+  });
+
+  it('shows loading component when calculation not resolved', () => {
+    expect(debugElement.query(By.css('app-loading'))).toBeTruthy();
+  });
+
+  it('shows results component when id is valid', () => {
+    component.calculationResults$ = of(mockResponse);
+    fixture.detectChanges();
+    expect(debugElement.query(By.css('app-tertiary-to-multi-results'))).toBeTruthy();
+  });
+
+  it('shows error component when id is valid', () => {
+    component.calculationResults$ = throwError(() => new Error(''));
+    component.error = 'error';
+    fixture.detectChanges();
+    expect(debugElement.query(By.css('app-error'))).toBeTruthy();
+  });
+});

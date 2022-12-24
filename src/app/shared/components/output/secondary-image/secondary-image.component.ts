64185587a2164789cc6baa48d0d2f44ec88420f3
Gabriel Wachowski <wachowski.gabriel@gmail.com>
2022-12-24 11:00:29 +0100

    feat: add pretty names to visualization tools

--- src/app/shared/components/output/secondary-image/secondary-image.component.ts
+++ src/app/shared/components/output/secondary-image/secondary-image.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { DrawingResult, ImageInformation } from 'src/app/shared/models/output/secondary-output.model';
 import { SelectField } from 'src/app/shared/models/select/select-field.model';
+import { VISUALIZATION_TOOL } from '../../../constants/param-options.const';
 import { ControlValueComponent, ControlValueProvider } from '../../control-value/control-value.component';
 
 
@@ -16,4 +17,8 @@ export class SecondaryImageComponent extends ControlValueComponent<SelectField> 
   DrawingResult: typeof DrawingResult = DrawingResult;
 
   constructor() { super(new SelectField(false)); }
+
+  getVisualizationName(rawName: string): string {
+    return VISUALIZATION_TOOL.find(item => item.key === rawName)?.label ?? rawName;
+  }
 }

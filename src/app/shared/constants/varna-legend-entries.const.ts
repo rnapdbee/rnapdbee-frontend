3d86b691e14a2dadb17c9db84aea035c4503a284
Gabriel Wachowski <gabriel.wachowski@student.put.poznan.pl>
2022-12-31 13:10:20 +0100

    feat: add legend for VARNA visualization tool (#100)
    * feat: varna legend component
    
    * chore: footer and shortened labels
    
    * chore: add anchor to /help link

---
+++ src/app/shared/constants/varna-legend-entries.const.ts
@@ -0,0 +1,79 @@
+export interface LegendEntry {
+  name: string,
+  pathToSVG: string,
+}
+
+export const VARNA_LEGEND_ENTRIES: LegendEntry[] = [
+  {
+    name: 'cWW',
+    pathToSVG: '/assets/varna-symbols/symbol-cww.svg',
+  },
+  {
+    name: 'tWW',
+    pathToSVG: '/assets/varna-symbols/symbol-tww.svg',
+  },
+  {
+    name: 'cWH',
+    pathToSVG: '/assets/varna-symbols/symbol-cwh.svg',
+  },
+  {
+    name: 'tWH',
+    pathToSVG: '/assets/varna-symbols/symbol-twh.svg',
+  },
+  {
+    name: 'cWS',
+    pathToSVG: '/assets/varna-symbols/symbol-cws.svg',
+  },
+  {
+    name: 'tWS',
+    pathToSVG: '/assets/varna-symbols/symbol-tws.svg',
+  },
+  {
+    name: 'cHW',
+    pathToSVG: '/assets/varna-symbols/symbol-chw.svg',
+  },
+  {
+    name: 'tHW',
+    pathToSVG: '/assets/varna-symbols/symbol-thw.svg',
+  },
+  {
+    name: 'cHH',
+    pathToSVG: '/assets/varna-symbols/symbol-chh.svg',
+  },
+  {
+    name: 'tHH',
+    pathToSVG: '/assets/varna-symbols/symbol-thh.svg',
+  },
+  {
+    name: 'cHS',
+    pathToSVG: '/assets/varna-symbols/symbol-chs.svg',
+  },
+  {
+    name: 'tHS',
+    pathToSVG: '/assets/varna-symbols/symbol-ths.svg',
+  },
+  {
+    name: 'cSW',
+    pathToSVG: '/assets/varna-symbols/symbol-csw.svg',
+  },
+  {
+    name: 'tSW',
+    pathToSVG: '/assets/varna-symbols/symbol-tsw.svg',
+  },
+  {
+    name: 'cSH',
+    pathToSVG: '/assets/varna-symbols/symbol-csh.svg',
+  },
+  {
+    name: 'tSH',
+    pathToSVG: '/assets/varna-symbols/symbol-tsh.svg',
+  },
+  {
+    name: 'cSS',
+    pathToSVG: '/assets/varna-symbols/symbol-css.svg',
+  },
+  {
+    name: 'tSS',
+    pathToSVG: '/assets/varna-symbols/symbol-tss.svg',
+  },
+];

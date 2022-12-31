18fb2de41d2ee7eb8d411309cb19159a26814cd5
Gabriel Wachowski <wachowski.gabriel@gmail.com>
2022-12-31 12:35:42 +0100

    chore: reduce nested if statements

--- src/app/shared/services/error/error.service.ts
+++ src/app/shared/services/error/error.service.ts
@@ -7,12 +7,12 @@ import { RNApdbeeError } from '../../models/error/error.model';
 })
 export class ErrorService {
   getErrorMessage(error: Error | HttpErrorResponse): string {
-    if ('error' in error) { // HttpErrorResponse
-      if ('message' in error.error) {
-        return `${(error.error as RNApdbeeError).message}`;
-      }
+    if (!('error' in error)) {
+      return `${error.message}`;
+    }
+    if (!('message' in error.error)) {
       return `${error.name}: ${error.statusText}`;
     }
-    return `${error.message}`;
+    return `${(error.error as RNApdbeeError).message}`;
   }
 }

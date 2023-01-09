96195156358bbf0b69a8d3abdc15cec1124d031b
Gabriel Wachowski <wachowski.gabriel@gmail.com>
2023-01-09 21:48:50 +0100

    feat: add more informative svg viewer state

--- src/app/shared/components/svg-viewer/svg-viewer.component.ts
+++ src/app/shared/components/svg-viewer/svg-viewer.component.ts
@@ -1,5 +1,6 @@
 import { AfterViewInit, Component, ElementRef, Input, ViewChild } from '@angular/core';
 import * as L from 'leaflet';
+import { HOST } from 'src/environments/environment';
 
 @Component({
   selector: 'app-svg-viewer[src]',
@@ -14,6 +15,8 @@ export class SvgViewerComponent implements AfterViewInit {
   private map: L.Map | undefined;
   private readonly bounds: L.LatLngBoundsExpression = [[0, 0], [150, 150]];
 
+  loading = true;
+  error = false;
 
   ngAfterViewInit(): void {
     if (!this.mapElement) {
@@ -22,10 +25,36 @@ export class SvgViewerComponent implements AfterViewInit {
 
     this.map = new L.Map(this.mapElement, {
       crs: L.CRS.Simple,
+      zoomSnap: 0.1,
+      zoomDelta: 0.25,
     });
-    L.imageOverlay(this.src, this.bounds).addTo(this.map);
-    this.map.attributionControl.remove();
-    this.map.attributionControl.setPrefix(false);
+
+    const svgOverlay = L.imageOverlay(`${HOST}${this.src}`, this.bounds);
+    svgOverlay.addTo(this.map);
     this.map.fitBounds(this.bounds);
+
+    this.removeAttribution(this.map);
+    this.handleOverlayLoading(svgOverlay);
+    this.handleOverlayError(this.map, svgOverlay);
+  }
+
+  private removeAttribution(map: L.Map): void {
+    map.attributionControl.remove();
+    map.attributionControl.setPrefix(false);
+  }
+
+  private handleOverlayLoading(overlay: L.ImageOverlay): void {
+    overlay.addEventListener('load', () => {
+      this.loading = false;
+    });
+  }
+
+  private handleOverlayError(map:L.Map, overlay: L.ImageOverlay): void {
+    overlay.addEventListener('error', () => {
+      this.loading = false;
+      this.error = true;
+      this.mapElement?.style.setProperty('height', '200px');
+      map.remove();
+    });
   }
 }

2552d9ff037ccb72a1090c6cc81fb8914b7633a2
Gabriel Wachowski <wachowski.gabriel@gmail.com>
2023-02-04 18:15:50 +0100

    chore: remove boilerplate code

--- src/app/informations/components/about-content/about-content.component.ts
+++ src/app/informations/components/about-content/about-content.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-about-content',
   templateUrl: './about-content.component.html',
-  styleUrls: ['./about-content.component.scss']
+  styleUrls: ['./about-content.component.scss'],
 })
-export class AboutContentComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class AboutContentComponent {}

96fd9ace146d2acd9432f1649803416d880dc7ab
Tomasz Zok <tomasz.zok@cs.put.poznan.pl>
2023-02-08 18:40:27 +0100

    feat: fill content of "Help" page

--- src/app/informations/pages/help/help-page.component.ts
+++ src/app/informations/pages/help/help-page.component.ts
@@ -7,7 +7,6 @@ import { TableOfContentsEntry } from 'src/app/shared/models/information/table-of
   styleUrls: ['./help-page.component.scss'],
 })
 export class HelpPageComponent {
-  // TODO: update this according to document struture
   helpSections: TableOfContentsEntry[] = [
     {
       name: 'General information',
@@ -19,14 +18,59 @@ export class HelpPageComponent {
       link: 'how-to-use',
       children: [
         {
-          name: '3D scenario',
+          name: '3D → (...)',
           link: '3d-scenario',
         },
         {
-          name: '2D scenario',
+          name: '2D → (...)',
           link: '2d-scenario',
+        },
+        {
+          name: '3D → multi 2D',
+          link: '3d-multi-2d',
         },
       ],
     },
+    {
+      name: 'Changes in RNApdbee 3.0',
+      link: 'v3-changes',
+      children: []
+    },
+    {
+      name: 'RNApdbee routines',
+      link: 'routines',
+      children: [
+        {
+          name: 'Base-pair identification',
+          link: 'bps-identification',
+        },
+        {
+          name: 'Handling of missing residues',
+          link: 'missing'
+        },
+        {
+          name: 'Handling of non-canonical interactions',
+          link: 'non-canonical'
+        },
+        {
+          name: 'Secondary structure drawing',
+          link: '2d-drawing'
+        },
+        {
+          name: 'Multiple secondary structures',
+          link: 'multiple-explanation'
+        }
+      ]
+    },
+    {
+      name: 'Supported file formats',
+      link: 'formats',
+      children: []
+    },
+    {
+      name: 'System requirements',
+      link: 'requirements',
+      children: []
+    }
   ];
 }

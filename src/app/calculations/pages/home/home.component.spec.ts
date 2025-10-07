112b20da3f1895cd4cdef71834b7f75e21bcd573
StormageddonDarkLordOfAll <jmp@onet.com.pl>
2025-10-07 18:09:34 +0200

    home.component.spec.ts removed unexpected block body

--- src/app/calculations/pages/home/home.component.spec.ts
+++ src/app/calculations/pages/home/home.component.spec.ts
@@ -24,9 +24,7 @@ describe('HomeComponent', () => {
           provide: ActivatedRoute,
           useValue: {
             queryParamMap: of({
-              get: (key: string) => {
-                return key === 'minimalView' ? 'false' : null;
-              },
+              get: (key: string) => key === 'minimalView' ? 'false' : null,
             }),
           },
         },
